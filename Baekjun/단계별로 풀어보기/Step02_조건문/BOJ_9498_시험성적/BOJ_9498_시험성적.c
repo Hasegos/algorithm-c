93523ca618aab721c4041538efaa219722865bba
Hasegos <csw020106@naver.com>
2026-03-20 18:35:30 +0900

    ADD [시험 성적](조건문) - 9498

---
+++ Baekjun/단계별로 풀어보기/Step02_조건문/BOJ_9498_시험성적/BOJ_9498_시험성적.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int A;
+    scanf("%d",&A);
+    if(A > 89 && A <= 100){
+        printf("A");
+    }
+    else if(A > 79 ){
+        printf("B");
+    }
+    else if(A > 69){
+        printf("C");
+    }    
+    else if(A > 59){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+}
