0f3643aadd37160a084cbb8f108782741b122a71
Hasegos <csw020106@naver.com>
2026-03-20 23:56:54 +0900

    ADD [최소, 최대](1차원 배열) - 10818

---
+++ Baekjun/단계별로 풀어보기/Step04_1차원배열/BOJ_10818_최소,최대/BOJ_10818_최소,최대.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int N,a,max = -1000000, min = 1000000;
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &a);
+
+        if(max < a){
+            max = a ;
+        }
+        if(min > a){
+            min = a;
+        }
+    }
+    printf("%d %d",min, max);
+}
