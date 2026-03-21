52111cdc3928968d3bb87af1c3316b665748e704
Hasegos <csw020106@naver.com>
2026-03-21 11:22:47 +0900

    ADD [문자열 반복](문자열) - 2675

---
+++ Baekjun/단계별로 풀어보기/Step05_문자열/BOJ_2675_문자열반복/BOJ_2675_문자열반복.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int T,R; 
+  char S[20];
+  scanf("%d",&T);  
+  for(int i = 0; i < T; i++){
+    scanf("%d %s",&R,S);
+    for(int k = 0; k < strlen(S); k++){
+        for(int j = 0; j < R; j++){
+            printf("%c",S[k]);
+        }
+    }
+    printf("\n");
+  }
+}
